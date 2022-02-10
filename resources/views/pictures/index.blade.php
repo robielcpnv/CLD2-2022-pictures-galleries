2d434bfb76ecadc72c37bf66e285a6e001bbbe21
Pascal Hurni <phi@pragmaticpurist.org>
2022-02-10 09:00:50 +0100

    Fix typo

--- resources/views/pictures/index.blade.php
+++ resources/views/pictures/index.blade.php
@@ -12,7 +12,7 @@
         @foreach ($pictures as $picture)
             <tr>
                 <td>{{ $picture->title }} </td>
-                <td><img src="{{ Storage::url($picture->path) }}"width="50" height="60"></td>
+                <td><img src="{{ Storage::url($picture->path) }}" width="50" height="60"></td>
             </tr>
         @endforeach
     </tbody>
